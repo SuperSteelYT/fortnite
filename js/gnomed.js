ef2cbbcbf22d04e6588a66a64f8eaa5f736675f0
Super_ <awesomegamer156@gmail.com>
2019-03-30 21:06:04 -0400

    gnomed

---
+++ js/gnomed.js
@@ -0,0 +1,5 @@
+function gnome()
+{
+	let gnomed = "Hulio there my old chum! I'm gnot a gnelf... I'm gnot a gnoblin... I'm a gnome, and you've been gnomed! <br />";
+	document.getElementById("gnomed").innerHTML = gnomed.repeat(100);
+}
